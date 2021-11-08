b4bf2935636a3fedf4c46f1df2b331a0476a6e44
Santiago <sberoch@gmail.com>
2021-11-08 18:14:41 -0300

    deploy

--- src/components/units/units.create.tsx
+++ src/components/units/units.create.tsx
@@ -1,71 +1,76 @@
 import { Box, Button, TextField, Typography } from "@material-ui/core";
-import SaveIcon from '@mui/icons-material/Save';
+import SaveIcon from "@mui/icons-material/Save";
 import { useEffect, useState } from "react";
-import {
-  ArrayInput, FormWithRedirect, SelectArrayInput
-} from "react-admin";
+import { ArrayInput, FormWithRedirect, SelectArrayInput } from "react-admin";
 import { useHistory } from "react-router-dom";
 import api from "../../api/axios";
-
 
 export const UnitsCreate = (props: any) => {
   const [title, setTitle] = useState("");
   const [lessons, setLessons] = useState([]);
-  const [lessonsChosen, setLessonsChosen] = useState<string[]>([])
+  const [lessonsChosen, setLessonsChosen] = useState<string[]>([]);
   let history = useHistory();
 
   useEffect(() => {
     async function fetchLessons() {
-      const response = await api.get('lessons');
-      setLessons(response.data.items)
-      console.log(lessons)
+      const response = await api.get("lessons");
+      setLessons(response.data.items);
+      console.log(lessons);
     }
     fetchLessons();
-  }, [])
+  }, []);
 
   const addLessonChosen = (event: any) => {
     setLessonsChosen((prev) => {
-      console.log(event.target.value)
-      return event.target.value
-    })
-  }
+      console.log(event.target.value);
+      return event.target.value;
+    });
+  };
 
   const handleTitleChange = (e: any) => {
-    setTitle(e.target.value)
-  }
-
+    setTitle(e.target.value);
+  };
 
   const handleSubmit = async () => {
     //Postear al back
-    const res = await api.post('/units', {
+    const res = await api.post("/units", {
       title,
-      lessonsIds: lessonsChosen.map(actual => +actual)
-    })
+      lessonsIds: lessonsChosen.map((actual) => +actual),
+    });
     console.log(res);
     //Redirect
     history.push("/units");
-  }
+  };
 
   return (
-    <FormWithRedirect {...props}
-      render={formProps => (
+    <FormWithRedirect
+      {...props}
+      render={(formProps) => (
         // here starts the custom form layout
         <form>
           <Box
             sx={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              flexDirection: 'column',
+              display: "flex",
+              alignItems: "flex-start",
+              flexDirection: "column",
               p: 1,
               m: 1,
               paddingTop: 20,
-              paddingLeft: 20
+              paddingLeft: 20,
             }}
           >
-            <Box sx={{ paddingBottom: 35  }}>
-              <Typography variant="h6" gutterBottom>Crear nueva unidad</Typography>
+            <Box sx={{ paddingBottom: 35 }}>
+              <Typography variant="h6" gutterBottom>
+                Crear nueva unidad
+              </Typography>
             </Box>
-            <TextField id="filled-basic" label="Title" variant="filled" value={title} onChange={handleTitleChange} />
+            <TextField
+              id="filled-basic"
+              label="Title"
+              variant="filled"
+              value={title}
+              onChange={handleTitleChange}
+            />
             <Box sx={{ paddingTop: 35, minWidth: 220, width: "auto" }}>
               <ArrayInput source="Lecciones">
                 <SelectArrayInput
@@ -75,7 +80,6 @@ export const UnitsCreate = (props: any) => {
                   variant="outlined"
                   label="Elegí una leccion"
                   onChange={addLessonChosen}
-
                 />
               </ArrayInput>
             </Box>
@@ -84,17 +88,17 @@ export const UnitsCreate = (props: any) => {
                 borderRadius: 35,
                 backgroundColor: "lightBlue",
                 padding: "18px 36px",
-                fontSize: "18px"
+                fontSize: "18px",
               }}
               onClick={handleSubmit}
               variant="contained"
-              startIcon={<SaveIcon />} >
+              startIcon={<SaveIcon />}
+            >
               Agregar
             </Button>
-
           </Box>
         </form>
       )}
     />
-  )
-}
+  );
+};
